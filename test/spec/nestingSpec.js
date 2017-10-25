5b143a97ace274a8173fcd234a9b02551d10415e
brennongs <brennonschow@gmail.com>
2017-10-25 15:47:12 -0600

    small testing fix

--- test/spec/nestingSpec.js
+++ test/spec/nestingSpec.js
@@ -1,7 +1,7 @@
 describe('Update Employee Info', function() {
     var returnedArr;
     it('Theo Trill object should be removed', function() { 
-        returnedArr = employeeUpdater();
+        returnedArr = employeeUpdater(employees);
         var flag = true;
         returnedArr.map(function(employee) {
             if (employee.firstName === 'Theo') flag = false;
